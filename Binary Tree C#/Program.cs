d744a584186ec60ede49db2c7338a7c2667f23a8
Jative <84320749+Jative@users.noreply.github.com>
2022-09-06 01:52:57 +0500

    Add files via upload

---
+++ Binary Tree C#/Program.cs
@@ -0,0 +1,113 @@
+﻿namespace Lab8_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree t = new Tree();
+
+            Console.WriteLine("Создан пустой корневой элемент\n");
+            Console.WriteLine("Команды:");
+            Console.WriteLine("1. Добавить элемент");
+            Console.WriteLine("2. Редактировать элемент");
+            Console.WriteLine("3. Удалить элемент");
+            Console.WriteLine("4. Сумма чисел, кратных 11");
+            Console.WriteLine("5. Вывести дерево");
+            Console.WriteLine("0. Завершить программу");
+
+            bool running = true;
+            while(running)
+            {
+                Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+                try
+                {
+                    Console.Write("Команда: ");
+                    string command = Console.ReadLine();
+
+                    switch (command)
+                    {
+                        case "1":
+                            Console.Write("Значение добавляемого элемента: ");
+                            string strVal = Console.ReadLine();
+                            try
+                            {
+                                int.Parse(strVal);
+                            } catch { throw new Exception("Для добавления доступны только целые числа!"); }
+                            int val = int.Parse(strVal);
+                            strVal = val.ToString();
+
+                            t.Insert(strVal);
+                            Console.WriteLine("Элемент добавлен успешно");
+                            break;
+
+                        case "2":
+                            Console.Write("Старое значение: ");
+                            string oldVal = Console.ReadLine();
+
+                            Console.Write("Новое значение: ");
+                            string newVal = Console.ReadLine();
+                            try
+                            {
+                                int.Parse(newVal);
+                            }
+                            catch { throw new Exception("Элементом дерева может быть только число!"); }
+                            int value = int.Parse(newVal);
+                            strVal = value.ToString();
+
+                            t.SetValue(oldVal, newVal);
+                            Console.WriteLine("Значение изменено успешно");
+                            break;
+
+                        case "3":
+                            Console.Write("Значение удаляемого элемента: ");
+                            string delVal = Console.ReadLine();
+
+                            t.Remove(delVal);
+                            Console.WriteLine("Элемент удалён успешно");
+                            break;
+
+                        case "4":
+                            int sum = 0, count = 0;
+
+                            string[] strVals = t.Display(t).TrimEnd().Split(' ');
+                            foreach (string strval in strVals)
+                            {
+                                try
+                                {
+                                    int intVal = int.Parse(strval);
+                                    if (intVal % 11 == 0)
+                                    {
+                                        sum += intVal;
+                                        count++;
+                                    }
+                                } catch { }
+                            }
+
+                            if (count == 0)
+                            {
+                                Console.WriteLine("В дереве нет чисел, кратных 11!");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Сумма: {sum}");
+                            }
+                            break;
+
+                        case "5":
+                            Console.WriteLine(t.Draw(t));
+                            break;
+
+                        case "0":
+                            running = false;
+                            Console.WriteLine("Программа завершена");
+                            break;
+
+                        default:
+                            Console.WriteLine("Неизвестная команда!");
+                            break;
+                    }
+                } catch (Exception ex) { Console.WriteLine(ex.Message); }
+            }
+        }
+    }
+}
---
+++ Binary Tree C#/Tree.cs
@@ -0,0 +1,207 @@
+﻿namespace Lab8_14
+{
+    class Tree
+    {
+        private string value;
+        private int count;
+        private Tree left;
+        private Tree right;
+
+        public void Insert(string value)
+        {
+            if (this.value == null)
+                this.value = value;
+            else
+            {
+                if (int.Parse(value) < int.Parse(this.value))
+                {
+                    if (left == null)
+                        this.left = new Tree();
+                    left.Insert(value);
+                }
+                else if (int.Parse(value) > int.Parse(this.value))
+                {
+                    if (right == null)
+                        this.right = new Tree();
+                    right.Insert(value);
+                }
+                else
+                    throw new Exception("Узел уже существует");
+            }
+
+            this.count = Recount(this);
+        }
+
+        public Tree Search(string value)
+        {
+            if (this.value == value)
+                return this;
+            else if (this.value.CompareTo(value) == 1)
+            {
+                if (left != null)
+                    return this.left.Search(value);
+                else
+                    throw new Exception("Искомого узла в дереве нет");
+            }
+            else
+            {
+                if (right != null)
+                    return this.right.Search(value);
+                else
+                    throw new Exception("Искомого узла в дереве нет");
+            }
+        }
+
+        public string Display(Tree t)
+        {
+            if (this == t && this.value == null)
+            {
+                return "В дереве нет элементов";
+            }
+            else if (this == null)
+            {
+                return "";
+            }
+            string result = "";
+            if (t.left != null)
+                result += Display(t.left);
+
+            result += t.value + " ";
+
+            if (t.right != null)
+                result += Display(t.right);
+
+            return result;
+        }
+
+        private int Recount(Tree t)
+        {
+            int count = 0;
+
+            if (t.left != null)
+                count += Recount(t.left);
+
+            count++;
+
+            if (t.right != null)
+                count += Recount(t.right);
+
+            return count;
+        }
+
+        public void Remove(string value)
+        {
+            Tree t = Search(value);
+            string[] str1 = Display(t).TrimEnd().Split(' ');
+            string[] str2 = new string[str1.Length - 1];
+
+            int i = 0;
+            foreach (string s in str1)
+            {
+                if (s != value)
+                    str2[i++] = s;
+            }
+
+            t.Clear();
+            foreach (string s in str2)
+                t.Insert(s);
+
+            this.count = Recount(this);
+        }
+
+        public void SetValue(string oldValue, string newValue)
+        {
+            Remove(oldValue);
+            try
+            {
+                Insert(newValue);
+                this.count = Recount(this);
+            } catch { Insert(oldValue); }
+        }
+
+        public void Clear()
+        {
+            this.value = null;
+            this.left = null;
+            this.right = null;
+        }
+
+        public string Draw(Tree t)
+        {
+            if (this == t && this.value == null)
+            {
+                return "В дереве нет элементов";
+            }
+            else if (this == null)
+            {
+                return "";
+            }
+
+            string result = t.value;
+            if (t.left != null)
+            {
+                result += "\n" + Draw(t.left);
+
+                string[] lines = result.Split('\n');
+                result = "";
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    lines[i] += new string(' ', t.value.Length);
+                }
+
+                lines[0] = new string(' ', lines[1].Length - t.value.Length) + t.value;
+
+                foreach (string line in lines)
+                {
+                    result += line + "\n";
+                }
+            }
+
+            if (t.right != null)
+            {
+                string[] leftLines = { t.value };
+                if (result.Contains("\n"))
+                {
+                    leftLines = result.Split('\n').Skip(1).ToArray();
+                }
+
+                string[] rightLines = { t.right.Draw(t.right) };
+                if (rightLines[0].Contains("\n"))
+                {
+                    rightLines = rightLines[0].Split('\n');
+                }
+                int leftLength = result.Split('\n')[0].Length;
+                int rightLength = rightLines[rightLines.Length - 1].Length;
+                int maxLines = leftLines.Length > rightLines.Length ? leftLines.Length : rightLines.Length;
+
+                string[] lines = new string[maxLines];
+                for (int i = 0; i < maxLines; i++)
+                {
+                    bool leftAdded = false, rightAdded = false;
+                    string line = "";
+                    try
+                    {
+                        line = leftLines[i];
+                        leftAdded = true;
+                    } catch { line = new string(' ', leftLength); }
+
+                    try
+                    {
+                        line += rightLines[i] + new string(' ', rightLength - rightLines[i].Length);
+                    }
+                    catch { line += new string(' ', rightLength); }
+
+                    lines[i] = line;
+                }
+                result = result.Split('\n')[0] + new string(' ', rightLength);
+                for (int i = 0; i < maxLines; i++)
+                {
+                    result += "\n" + lines[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
